0e7f64d4ff3c872549f9cb9b3e892237c88f7b63
jinho <52993842+birariro@users.noreply.github.com>
2024-01-23 17:34:56 +0900

    Update commit-msg message

--- git/commit-msg.sh
+++ git/commit-msg.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 COMMIT_MSG_FILE=$1
-
-#커밋 메시지중 1번 라인을 얻는다.
 FIRST_LINE=`head -n1 ${COMMIT_MSG_FILE}`
 
 byPass() {
@@ -42,13 +40,13 @@ validHeader() {
     echo "\n[commit lint error] HEADER 의 type, 콜론, 띄어쓰기 형태를 확인\n"
 
     echo "<type>(scope option): <subject>"
-    echo "- feat: 새로운 기능 추가"
+    echo "- feat: 신규 기능 추가"
     echo "- fix: 버그 수정"
-    echo "- docs: 문서의 수정"
-    echo "- style: (코드의 수정 없이) 스타일(style)만 변경(들여쓰기 같은 포맷이나 세미콜론을 빼먹은 경우)"
-    echo "- refactor: 코드를 리펙토링"
-    echo "- test: Test 관련한 코드의 추가, 수정"
-    echo "- chore: (코드의 수정 없이) 설정을 변경"
+    echo "- docs: 문서 수정"
+    echo "- refactor: 코드 리펙토링"
+    echo "- test: Test 관련한 코드 추가 혹은 수정"
+    echo "- style: 코드의 수정 없이 스타일만 변경 (들여쓰기 수정, 세미콜론 누락 수정 등)"
+    echo "- chore: 코드의 수정 없이 설정만 변경 (빌드 번호 수정, 패키지 매니저 수정 등)"
     exit 1
 
   fi
